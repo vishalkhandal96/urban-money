f8036a0ee43a26f09f645e6490d3ec5819012ef1
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-08-09 17:08:29 +0530

    update in modal box

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-let navbar = document.querySelector(".navbar");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-document.querySelector(".nav_toggle__menu").addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
+// Get the button that opens the modal
+var personalLoan = document.getElementById("personalLoan");
 
-document.querySelector(".nav_toggle__close").addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
+var homeLoan = document.getElementById("homeLoan");
+
+var loanAgainstProperty = document.getElementById("loanAgainstProperty");
+
+var businessLoan = document.getElementById("businessLoan");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+personalLoan.onclick = function () {
+  modal.style.display = "block";
+};
+
+homeLoan.onclick = function () {
+  modal.style.display = "block";
+};
+
+loanAgainstProperty.onclick = function () {
+  modal.style.display = "block";
+};
+
+businessLoan.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
